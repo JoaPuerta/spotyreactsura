194e9c222a34c7cb9f37ddc3288b3a955c53554e
407 <407@academicos.cesde.edu.co>
2022-10-19 17:20:17 -0500

    se pinta la musica xD

--- src/Albumes/Albumes.js
+++ src/Albumes/Albumes.js
@@ -1,43 +1,49 @@
-import { useState, useEffect } from "react";
-import { servicioSpoty } from "../services/servicioSpoty.js";
-import { servicioTOKEN } from "../services/servicioTOKEN.js";
-
+import { useState, useEffect } from 'react'
+import { servicioSpoty } from '../services/servicioSpoty.js'
+import { servicioTOKEN } from '../services/servicioTOKEN.js'
 
 export function Albumes() {
   //declarando mi primer useState
-  const [canciones, setCanciones] = useState(null);
+  const [canciones, setCanciones] = useState(null)
 
   //useState para la carga de datos
-  const [carga, setCarga] = useState(true);
+  const [carga, setCarga] = useState(true)
 
   //usando el usseEffect
-  useEffect(function(){
-
-    servicioSpoty()
-    .then(function (respuesta) {
-      setCanciones(respuesta.tracks);
+  useEffect(function () {
+    servicioSpoty().then(function (respuesta) {
+      setCanciones(respuesta.tracks)
       setCarga(false)
-    });
-  
-    console.log(canciones);    
-  },[])
+    })
 
-  if (carga==true) {
+    console.log(canciones)
+  }, [])
 
-    return(
-        <>
-            <h1>Estoy cargando...</h1>
-        </>
-    )
-    
-  } else {
-
+  if (carga == true) {
     return (
-        <>
-          <h1>Hola soy albumes y soy un componente</h1>
-        </>
-      );
-    
+      <>
+        <h1>Estoy cargando...</h1>
+      </>
+    )
+  } else {
+    return (
+      <>
+        <div className="row row-cols-1 row-cols-md-4 g-3 justify-content-center mt-5">
+          {canciones.map(function (cancion) {
+            return (
+              <>
+                <div className="col mt-3 justify-content-center">
+                  <div className="card h-100">
+                    <audio className='mt-2 ms-5' src={cancion.preview_url} controls />
+                    <h3 className="text-center">{cancion.name}</h3>
+                    <img src={cancion.album.images[0].url} />
+                  </div>
+                </div>
+              </>
+            )
+          })}
+        </div>
+      </>
+    )
   }
-  
 }
